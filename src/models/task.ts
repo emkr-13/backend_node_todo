75467cdce34e8ae33c05867ba4b46bbc414a281b
emkr-13 <krisna.rata13@gmail.com>
2025-05-27 10:17:44 +0700

    Feat: Add task model to the database schema

--- src/models/task.ts
+++ src/models/task.ts
@@ -5,14 +5,15 @@ import {
   boolean,
   varchar,
   timestamp,
+  uuid,
 } from "drizzle-orm/pg-core";
 import { users } from "./user";
-export const todo = pgTable("task", {
+export const task = pgTable("task", {
   id: serial("id").primaryKey(),
   description: varchar("description", { length: 255 }),
   is_completed: boolean("is_completed").default(false),
   position: integer("position").default(0),
-  user_id: varchar("user_id")
+  user_id: uuid("user_id")
     .references(() => users.id, { onDelete: "restrict" })
     .notNull(),
   createdAt: timestamp("created_at").defaultNow(),
